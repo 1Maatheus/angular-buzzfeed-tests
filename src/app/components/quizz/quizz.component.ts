1ba1d72cd741899204233c25b188bf95f43da4d7
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2023-09-12 23:58:49 -0300

    Commit

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -19,6 +19,7 @@ export class QuizzComponent implements OnInit {
 
   playerChoice(value: string) {
     this.answers.push(value);
+    this.nextStep();
   }
 
   async nextStep() {
@@ -27,8 +28,27 @@ export class QuizzComponent implements OnInit {
     if (this.questionMaxIndex > this.questionIndex) {
       this.questionSelected = this.questions[this.questionIndex];
     } else {
+      const finalAnswer: string = await this.checkResult(this.answers);
       this.finished = true;
+      this.answerSelected =
+        quizz_questions.results[
+          finalAnswer as keyof typeof quizz_questions.results
+        ];
     }
+  }
+
+  async checkResult(answers: string[]) {
+    const result = answers.reduce((previous, current, index, arr) => {
+      if (
+        arr.filter((item) => item === previous).length >
+        arr.filter((item) => item === current).length
+      ) {
+        return previous;
+      } else {
+        return current;
+      }
+    });
+    return result;
   }
 
   ngOnInit(): void {
